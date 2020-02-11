dd011ae368d1a9c3c0f4599d6a7f1c05833587c5
Ernad Husremovic <hernad@bring.out.ba>
2020-02-11 20:54:25 +0100

    windows build debug

---
+++ windows_build_debug.cmd
@@ -0,0 +1,1 @@
+bazel-bazel_rules_graal\external\graal\bin\native-image.cmd -cp bazel-out/x64_windows-fastbuild/bin/example/libmain.jar "-H:Class=Main" "-H:Name=bazel-out/x64_windows-fastbuild/bin/example/main-native-bin"  -H:+ReportExceptionStackTraces
