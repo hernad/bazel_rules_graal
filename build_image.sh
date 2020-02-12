7e0c61f89d54c08725eae5e75255729530ca896d
hernad <hernad@bring.out.ba>
2020-02-12 09:43:31 +0100

    build jdk 11

--- build_image.sh
+++ build_image.sh
@@ -3,7 +3,7 @@
 cd bazel-bazel_rules_graal
 external/graal/bin/native-image -cp bazel-out/k8-fastbuild/bin/example/libmain.jar -H:Class="Main" -H:Name=bazel-out/k8-fastbuild/bin/example/main-native-bin-11 --native-compiler-path=/bin/gcc  --verbose
 
-cd
+cd ..
 
 
 ls -lh bazel-bin/example/main-native-bin-11
