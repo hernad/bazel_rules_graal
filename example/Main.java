a60f2d7c0a1e8535f6817b9c80ce0b7c3db9b962
hernad <hernad@bring.out.ba>
2020-02-12 15:48:21 +0100

    swing

--- example/Main.java
+++ example/Main.java
@@ -1,7 +1,29 @@
+import javax.swing.*; 
+
 public class Main {
+
+    private static void createAndShowGUI() {
+        //Create and set up the window.
+        JFrame frame = new JFrame("HelloWorldSwing");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        //Add the ubiquitous "Hello World" label.
+        JLabel label = new JLabel("Hello World");
+        frame.getContentPane().add(label);
+
+        //Display the window.
+        frame.pack();
+        frame.setVisible(true);
+    }
 
     public static void main(String args[]) {
         System.out.println("Hello, Stripe");
+
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                createAndShowGUI();
+            }
+        });
     }
 
 }
